21c37ca0649b0cceaa069a59d59882b83e20b461
Leah Sattar <143446653+LSattar@users.noreply.github.com>
2025-11-26 14:29:07 -0500

    Added repository annotations

--- backend/shelfsync/src/main/java/com/shelfsync/repositories/EmployeeRepository.java
+++ backend/shelfsync/src/main/java/com/shelfsync/repositories/EmployeeRepository.java
@@ -3,7 +3,9 @@ package com.shelfsync.repositories;
 import com.shelfsync.models.*;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, UUID>{
 
 }
--- backend/shelfsync/src/main/java/com/shelfsync/repositories/LocationRepository.java
+++ backend/shelfsync/src/main/java/com/shelfsync/repositories/LocationRepository.java
@@ -2,7 +2,9 @@ package com.shelfsync.repositories;
 
 import com.shelfsync.models.*;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LocationRepository extends JpaRepository<Location, Integer>{
 
 }
